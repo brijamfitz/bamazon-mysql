b176fbd9559e94fd3594bff1fa0753fe2010d972
Brian Fitzgerald <brianjfitzgerald@gmail.com>
2018-10-18 15:59:10 -0400

    work in progress

--- schema.sql
+++ schema.sql
@@ -34,4 +34,16 @@ VALUES ('Playstation 4', 'electronics', 400, 20);
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('Playstation 4', 'electronics', 400, 20);
 
+CREATE TABLE departments(
+    department_id INT(11) AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(50),
+    over_head_costs INT(11),
+    PRIMARY KEY (department_id)
+);
+
+ALTER TABLE products
+ADD product_sales DECIMAL(11,2);
+
 SELECT * FROM products;
+
+SELECT * FROM departments;
